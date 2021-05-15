b179aba74c2ded9acadb74528d9286ccacc2739f
Zach Raab <zpraab@hotmail.com>
2021-05-14 23:49:36 -0600

    index.js and generateHTML.js file creation, readme creation

---
+++ generateHTML.js
@@ -0,0 +1,23 @@
+const htmlTemplate = `!<HTML>
+
+`;
+
+// loop through array of all employees
+//      call getRole() and return card for that employee
+
+function employeeLoop(arrEmployees) {
+  arrEmployees.forEach((employee) => {
+    switch (employee.getRole()) {
+      case "Engineer":
+        engineerCard(employee);
+        break;
+    }
+  });
+
+  return (htmlTemplate += `</body></HTML>`);
+}
+
+function engineerCard(person) {
+  return (htmlTemplate += `
+    <div>${person.name}</div>`);
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+// require all classes ie. manager, engineer etc
+// require inquirer, fs
+
+//create class functions
+//   inquirer.prompt ask questions for each class
+//   calls 4th function
+
+// inquirer.promt what type of employee would you like to create next
+//   give array of choices of which class to create then call that class
+//   I'm done choice calls write file function
+
+// Write HTML file function
