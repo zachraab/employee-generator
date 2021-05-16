82e56c23e801445675464b4483f49075edbf4195
Zach Raab <zpraab@hotmail.com>
2021-05-16 01:47:00 -0600

    styling, animation, fix bugs in prompt questions, validate user input, html template, dynamic card generation functions, push new employee objects to team array

--- generateHTML.js
+++ generateHTML.js
@@ -1,25 +1,99 @@
-const htmlTemplate = `!<HTML>
+const htmlTemplate = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./dist/style.css" />
+    <title>Employee Generator</title>
+  </head>
+  <body>
+    <header>
+      <div id="header-bg"></div>
+      <div id="header-bg2"></div>
+      <div id="header-bg3"></div>
+      <h1>My Team</h1>
+    </header>
+    <main id="card-parent">
+      <div id="half-circle"></div>
+      <!-- blue stripe -->
+      <div id="bg4"></div>
+      <!-- circle -->
+      <div id="bg5"></div>
+      <div id="bg6"></div>
+
+      <!-- cards -->
 
 `;
 
 // loop through array of all employees
 //      call getRole() and return card for that employee
 
-function employeeLoop(arrEmployees) {
-  arrEmployees.forEach((employee) => {
+function employeeLoop(myTeamArray) {
+  myTeamArray.forEach((employee) => {
     switch (employee.getRole()) {
+      case "Manager":
+        managerCard(employee);
+        break;
+
       case "Engineer":
         engineerCard(employee);
+        break;
+
+      case "Intern":
+        internCard(employee);
         break;
     }
   });
 
-  return (htmlTemplate += `</body></HTML>`);
+  return (htmlTemplate += `</main>
+  </body>
+</html>`);
+}
+
+function managerCard(person) {
+  return (htmlTemplate += `
+  <div class="card">
+  <div class="card-header">
+    <h3>${person.name}</h3>
+    <p>${person.class}</p>
+  </div>
+  <ul>
+    <li>ID: ${person.id}</li>
+    <li>e-mail: ${person.email}</li>
+    <li>Office #: ${person.officeNumber}</li>
+  </ul>
+</div>`);
 }
 
 function engineerCard(person) {
   return (htmlTemplate += `
-    <div>${person.name}</div>`);
+  <div class="card">
+  <div class="card-header">
+    <h3>${person.name}</h3>
+    <p>${person.class}</p>
+  </div>
+  <ul>
+    <li>ID: ${person.id}</li>
+    <li>e-mail: ${person.email}</li>
+    <li>GitHub: <a href="https://github.com/${person.github}">${person.name}</a></li>
+  </ul>
+</div>`);
 }
 
-module.exports(employeeLoop);
+function internCard(person) {
+  return (htmlTemplate += `
+  <div class="card">
+  <div class="card-header">
+    <h3>${person.name}</h3>
+    <p>${person.class}</p>
+  </div>
+  <ul>
+    <li>ID: ${person.id}</li>
+    <li>e-mail: ${person.email}</li>
+    <li>School: ${person.school}</li>
+  </ul>
+</div>`);
+}
+
+module.exports = employeeLoop;
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
@@ -6,13 +5,165 @@ const generateHTML = require("./generateHTML");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+const myTeamArray = [];
+
 //create class functions
 //   inquirer.prompt ask questions for each class
 //   calls 4th function
+const managerQuestions = [
+  {
+    name: "class",
+    type: "confirm",
+    message: "Is this employee a manager?",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "What is the manager's name?",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "what is the manager's ID?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is the manager's email?",
+  },
+  {
+    name: "officeNumber",
+    type: "input",
+    message: "What is the manager's office number?",
+  },
+];
 
+const engineerQuestions = [
+  {
+    name: "class",
+    type: "confirm",
+    message: "Is this employee an engineer?",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "What is the engineer's name?",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "what is the engineer's id?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is the engineer's email?",
+  },
+  {
+    name: "github",
+    type: "input",
+    message: "What is the engineer's GitHub username?",
+  },
+];
+
+const internQuestions = [
+  {
+    name: "class",
+    type: "confirm",
+    message: "Is this employee an intern?",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "What is the intern's name?",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "what is the intern's id?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is the intern's email?",
+  },
+  {
+    name: "school",
+    type: "input",
+    message: "Where does the intern go to school?",
+  },
+];
+
+const newEmployee = [
+  {
+    name: "class",
+    type: "list",
+    choices: ["Engineer", "Intern", "No, finish roster."],
+    message: "Would you like to add an employee?",
+  },
+];
 // inquirer.promt what type of employee would you like to create next
 //   give array of choices of which class to create then call that class
 //   I'm done choice calls write file function
+engineerPrompt = () => {
+  inquirer.prompt(engineerQuestions).then((data) => {
+    if (data.class) {
+      data.class = "Engineer";
+    } else {
+      console.log("Error: Employee must be an Engineer");
+      return;
+    }
+
+    if (!data.name || !data.id || !data.email || !data.github) {
+      console.log("Error: Please answer all prompts");
+      return;
+    }
+
+    const newEngineer = new Engineer(
+      data.name,
+      data.id,
+      data.email,
+      data.github
+    );
+    myTeamArray.push(newEngineer);
+    console.log(myTeamArray);
+    buildTeam();
+  });
+};
+
+internPrompt = () => {
+  inquirer.prompt(internQuestions).then((data) => {
+    if (data.class) {
+      data.class = "Intern";
+    } else {
+      console.log("Error: Employee must be an Intern");
+      return;
+    }
+
+    if (!data.name || !data.id || !data.email || !data.school) {
+      console.log("Error: Please answer all prompts");
+      return;
+    }
+
+    const newIntern = new Intern(data.name, data.id, data.email, data.school);
+    myTeamArray.push(newIntern);
+    console.log(myTeamArray);
+    buildTeam();
+  });
+};
+
+buildTeam = () => {
+  inquirer.prompt(newEmployee).then((data) => {
+    if (data.class === "Engineer") {
+      engineerPrompt();
+    } else if (data.class === "Intern") {
+      internPrompt();
+    } else {
+      console.log("generate file");
+      generate("my-team.html", generateHTML(data));
+    }
+  });
+};
 
 // Write HTML file function
 const generate = (fileName, data) => {
@@ -22,10 +173,35 @@ const generate = (fileName, data) => {
       : console.log("'My Team' HTML file has been successfully created!")
   );
 };
-const init = () => {
-  inquirer.prompt(questions).then((data) => {
-    generate("my-team.html", generateHTML(data));
+
+const initManager = () => {
+  inquirer.prompt(managerQuestions).then((data) => {
+    if (data.class) {
+      data.class = "Manager";
+    } else {
+      console.log("Error: Employee must be a manager");
+      return;
+    }
+    const officeNumber = parseInt(data.officeNumber);
+    if (!data.name || !data.id || !data.email || !data.officeNumber) {
+      console.log("Error: Please answer all prompts");
+      return;
+    } else if (Number.isNaN(officeNumber)) {
+      console.log("Error: Office number must be a number");
+      return;
+    }
+
+    const newManager = new Manager(
+      data.name,
+      data.id,
+      data.email,
+      data.officeNumber
+    );
+    myTeamArray.push(newManager);
+    console.log(myTeamArray);
+
+    buildTeam();
   });
 };
 
-init();
+initManager();
